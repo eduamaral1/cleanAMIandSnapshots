fd363d49b96b82f813f4de825d6ea48c42aa4309
eduamaral1 <100854794+eduamaral1@users.noreply.github.com>
2023-02-07 08:35:43 -0300

    Terraform + Código
    Terraform com parâmetros para subir a função do AWS Lambda com o código em python + eventbridge com rule de 7 dias e IAM Role com permissão refinada apenas para executar a função.

---
+++ ami_cleanup.py
@@ -0,0 +1,75 @@
+import boto3
+from datetime import timedelta, datetime, timezone
+import logging
+import botocore
+
+# Intialize logging
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+
+def lambda_handler(event, context):
+    '''Clean AMIs and its associated SnapShots which are older than 7 Days and without "Exclude = No" tag in a AWS Region
+    Exempt AMI which is currently being used in AWS Launch Config'''
+
+    ec2 = boto3.client('ec2')
+    autoscaling = boto3.client('autoscaling')
+    ami_response = ec2.describe_images(Owners=['self'])
+    snapshot_response = ec2.describe_snapshots(OwnerIds=['self'])
+    lc_response = autoscaling.describe_launch_configurations()
+    amis = {}
+    amidnd = []
+    for i in ami_response['Images']:
+        for tag in i.get('Tags', ''):
+            if tag['Key'] == 'Exclude' and tag['Value'] == 'No' in tag.values():
+                amidnd.append(i.get('ImageId'))
+                break
+
+    for ami in lc_response['LaunchConfigurations']:
+        if ami['ImageId'] not in amidnd:
+            amidnd.append(ami['ImageId'])
+    for i in ami_response['Images']:
+        if i.get('Tags') == None or i['ImageId'] not in amidnd:
+            amis[i.get('ImageId')] = i.get('CreationDate')
+    if not amis:
+        logger.info('No AMIs and SnapShots found to be deregister')
+    else:
+        for ami, cdate in amis.items():
+            if cdate < (datetime.now(timezone.utc)-timedelta(days=7)).isoformat():
+                logger.info('De-registering...'+ami)
+                ec2.deregister_image(ImageId=ami)
+                for snapshot in snapshot_response['Snapshots']:
+                    if ami in snapshot.get('Description', ''):
+                        logger.info(
+                            'Deleting '+snapshot.get('SnapshotId') + "of "+ami)
+                        ec2.delete_snapshot(
+                            SnapshotId=snapshot.get('SnapshotId'))
+            continue
+
+    abandon_snap_clean(ami_response, snapshot_response)
+
+
+def abandon_snap_clean(ami_response, snapshot_response):
+    '''Clean abandon ebs snapshots of which no AMI has been found'''
+    ec2 = boto3.client('ec2')
+    snapdndids = []
+    for i in ami_response['Images']:
+        for snap in i['BlockDeviceMappings']:
+            if 'Ebs' in snap.keys():
+                snapdndids.append(snap['Ebs']['SnapshotId'])
+    for snapid in snapshot_response['Snapshots']:
+        if snapid['SnapshotId'] not in snapdndids:
+            try:
+                logger.info('Deleting abandon snapshots '+snapid['SnapshotId'])
+                ec2.delete_snapshot(SnapshotId=snapid['SnapshotId'])
+            except botocore.exceptions.ClientError as error:
+                if error.response['Error']['Code'] == 'InvalidSnapshot.InUse':
+                    logger.info(
+                        'SnapshotId '+snapid['SnapshotId']+' is already being used by an AMI')
+                else:
+                    raise error
+        else:
+            logger.info('No abandon EBS SnapShots found to clean up')
+            continue
+    else:
+        logger.info('No SnapShots found')
---
+++ main.tf
@@ -0,0 +1,66 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+locals {
+  function_name = "clean_amis_and_snapshots"
+}
+
+data "aws_iam_policy_document" "lambda_execution_policy" {
+  statement {
+    actions = [
+      "ec2:DescribeImages",
+      "ec2:DeregisterImage",
+      "ec2:DeleteSnapshot",
+      "autoscaling:DescribeLaunchConfigurations",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents"
+    ]
+    resources = ["*"]
+  }
+}
+
+resource "aws_lambda_function" "lambda" {
+  function_name = local.function_name
+  filename      = "ami_cleanup.zip"
+  handler       = "index.lambda_handler"
+  runtime       = "python3.9"
+  role          = aws_iam_role.lambda.arn
+}
+
+resource "aws_iam_role" "lambda" {
+  name = "lambda_execution_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        },
+        Effect = "Allow",
+        Sid    = ""
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy" "lambda_execution_policy" {
+  name = "lambda_execution_policy"
+  role = aws_iam_role.lambda.id
+
+  policy = data.aws_iam_policy_document.lambda_execution_policy.json
+}
+
+resource "aws_cloudwatch_event_rule" "clean_ami_snapshot_cron_rule" {
+  name                = "clean-ami-snapshot-cron-rule"
+  description         = "Eventbridge rule to trigger lambda function for cleaning AMIs and snapshots"
+  schedule_expression = "rate(7 days)"
+}
+
+resource "aws_cloudwatch_event_target" "lambda_target" {
+  rule      = aws_cloudwatch_event_rule.clean_ami_snapshot_cron_rule.name
+  arn       = aws_lambda_function.lambda.arn
+  target_id = "clean_amis_and_snapshots_target"
+}
